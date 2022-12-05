882a6f8fd28a5cec92afa2f1dcc9ff80bcbfe266
OJII3 <kawaisounayuriko@gmail.com>
2022-12-05 12:07:57 +0900

    add index.html, cmakefiles, simplecontroller

---
+++ controllers/http_simple_controller.cc
@@ -0,0 +1,6 @@
+#include "http_simple_controller.h"
+
+void http_simple_controller::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+}
